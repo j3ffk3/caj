f15ef5e75c900e1b6854d2a12c4885f716e504c3
Jeff Ke <>
2022-10-17 17:54:10 +0800

    [main] fix hostname api bug.

--- master-file/src/main/java/com/rh/caj/masterfile/ui/api/HostnameApi.java
+++ master-file/src/main/java/com/rh/caj/masterfile/ui/api/HostnameApi.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HostnameApi {
 
 	@GetMapping(value = "/api/hostname", produces = "application/json")
-	public ResponseEntity<GenericResponseDTO> getFare(@PathVariable("type") String type)
+	public ResponseEntity<GenericResponseDTO> getFare()
 			throws JsonMappingException, JsonProcessingException {
 		log.info("GET /api/hostname");
 		return new ResponseEntity<>(GenericResponseDTO.builder().data(Optional.ofNullable(System.getenv("HOSTNAME")))
